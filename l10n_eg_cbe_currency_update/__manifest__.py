880ac601dcc13ddafd27e5df952586e6a817453d
Khaled Hamed <khaled.hamed@grandtk.com>
2021-05-11 01:37:40 +0200

    Modify manifest file

--- l10n_eg_cbe_currency_update/__manifest__.py
+++ l10n_eg_cbe_currency_update/__manifest__.py
@@ -9,12 +9,12 @@
          """,
     "author": "GRANDTK",
     "website": "http://www.grandtk.com",
-
+    "license": "LGPL-3",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "Localization",
-    "version": "0.1",
+    "version": "14.0.0.1.0",
 
     # any module necessary for this one to work correctly
     "depends": ["base", "iap"],
